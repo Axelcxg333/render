2c3b45ef2da6af92c157248ac1267a1fb41d61ee
Axelcxg333 <axelcxg333@gmail.com>
2025-10-04 16:42:17 -0500

    Añadida pagina y funciones del formulario nuevo

--- src/main/java/com/tecsup/controller/CursoController.java
+++ src/main/java/com/tecsup/controller/CursoController.java
@@ -1,13 +1,20 @@
 package com.tecsup.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import com.tecsup.model.entities.Curso;
 import com.tecsup.services.CursoService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("curso")
@@ -17,6 +24,11 @@ public class CursoController {
     @Autowired
     private CursoService servicio;
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "redirect:/listar";
+    }
+
     @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public String listar(Model model) {
         model.addAttribute("titulo","Listado de cursos");
@@ -24,12 +36,25 @@ public class CursoController {
         return "listView";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "redirect:/listar";
+    @RequestMapping(value = "/form")
+    public String create(Map<String, Object> model) {
+        Curso curso = new Curso();
+        model.put("curso", curso);
+        model.put("titulo", "Formulario de Curso");
+        return "formView";
     }
 
-
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String save(@Valid Curso curso, BindingResult result, Model model, SessionStatus status) {
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Formulario de Curso");
+            return "formView";
+        }
+        
+        servicio.SaveCourse(curso);
+        status.setComplete();
+        return "redirect:listar";
+    }
 
 
 
